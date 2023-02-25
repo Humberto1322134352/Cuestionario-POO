bdcea70415b298a1364a49d8312c18c8db3ec434
Humberto1322134352 <112767622+Humberto1322134352@users.noreply.github.com>
2023-02-24 21:14:56 -0600

    Add files via upload

--- Cuestionariopoo/src/Main.java
+++ Cuestionariopoo/src/Main.java
@@ -1,30 +1,16 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        // Pedir los tamaños de las figuras al usuario
-        System.out.println("Ingrese el radio del circulo: ");
-        double radio = sc.nextDouble();
-
-        System.out.println("Ingrese la base del rectangulo: ");
-        double base = sc.nextDouble();
-
-        System.out.println("Ingrese la altura del rectangulo: ");
-        double altura = sc.nextDouble();
-
-        System.out.println("Ingrese el lado del cuadrado: ");
-        double lado = sc.nextDouble();
-
-        System.out.println("Ingrese la base del triangulo rectangulo: ");
-        double baseTriangulo = sc.nextDouble();
-
-        System.out.println("Ingrese la altura del triangulo rectangulo: ");
-        double alturaTriangulo = sc.nextDouble();
-
-
+        // Pedir los tamaños de las figuras al usuario utilizando JOptionPane
+        double radio = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el radio del circulo:"));
+        double base = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la base del rectangulo:"));
+        double altura = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la altura del rectangulo:"));
+        double lado = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el lado del cuadrado:"));
+        double baseTriangulo = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la base del triangulo:"));
+        double alturaTriangulo = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la altura del triangulo:"));
 
         // Crear las figuras con los tamaños ingresados
         Circulo circulo = new Circulo(radio);
@@ -33,32 +19,15 @@ public class Main {
         Triangulo triangulo = new Triangulo(baseTriangulo, alturaTriangulo);
 
         // Probar los métodos de cada figura
-        System.out.println("Circulo:");
-        System.out.println("Radio: " + circulo.getRadio());
-        System.out.println("Area: " + circulo.calcularArea());
-        System.out.println("Perimetro: " + circulo.calcularPerimetro());
+        JOptionPane.showMessageDialog(null,"Circulo:\n"+"Radio: "+circulo.getRadio()+"\nArea: "+circulo.calcularArea()+"\nPerimetro: "+circulo.calcularPerimetro());
 
-        System.out.println("Rectangulo:");
-        System.out.println("Base: " + rectangulo.getBase());
-        System.out.println("Altura: " + rectangulo.getAltura());
-        System.out.println("Area: " + rectangulo.calcularArea());
-        System.out.println("Perimetro: " + rectangulo.calcularPerimetro());
+        JOptionPane.showMessageDialog(null,"Rectangulo:\n"+"Base: "+rectangulo.getBase()+"\nAltura: "+rectangulo.getAltura()+"\nArea: "+rectangulo.calcularArea()+"\nPerimetro: "+rectangulo.calcularPerimetro());
 
-        System.out.println("Cuadrado:");
-        System.out.println("Lado: " + cuadrado.getLado());
-        System.out.println("Area: " + cuadrado.calcularArea());
-        System.out.println("Perimetro: " + cuadrado.calcularPerimetro());
+        JOptionPane.showMessageDialog(null,"Cuadrado:\n"+"Lado: "+cuadrado.getLado()+"\nArea: "+cuadrado.calcularArea()+"\nPerimetro: "+cuadrado.calcularPerimetro());
 
-        System.out.println("Triangulo:");
-        System.out.println("Base: " + triangulo.getBase());
-        System.out.println("Altura: " + triangulo.getAltura());
-        System.out.println("Area: " + triangulo.calcularArea());
-        System.out.println("Perimetro: " + triangulo.calcularPerimetro());
-        System.out.println("Hipotenusa: " + triangulo.calcularHipotenusa());
-        System.out.println("Tipo: " + triangulo.determinarTipoTriangulo());
-
-
-
+        JOptionPane.showMessageDialog(null,"Triangulo:\n"+"Base: "+triangulo.getBase()+"\nAltura: "+triangulo.getAltura()+"\nArea: "+triangulo.calcularArea()+"\nPerimetro: "+triangulo.calcularPerimetro()+"\nHipotenusa: "+triangulo.calcularHipotenusa()+"\nTipo: "+triangulo.determinarTipoTriangulo());
     }
-
 }
+
+
+
